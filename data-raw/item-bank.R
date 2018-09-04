4708405b3784af94d1adc62a75f24a7d3e795700
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-04 17:13:11 +0100

    Removing inst/

--- data-raw/item-bank.R
+++ data-raw/item-bank.R
@@ -1,5 +1,4 @@
-item_bank <- read.csv(system.file("item-bank.csv", package = "mdt", mustWork = TRUE),
-              stringsAsFactors = FALSE)
+item_bank <- read.csv("data-raw/item-bank.csv", stringsAsFactors = FALSE)
 item_bank$answer <- item_bank$oddity
 stopifnot(is.numeric(item_bank$answer))
 devtools::use_data(item_bank, overwrite = TRUE)
