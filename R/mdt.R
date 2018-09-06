7fb3cdc86c84a7cc7153657e7f87e7b868700bd0
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-07 00:01:35 +0100

    Working on build problem

--- R/mdt.R
+++ R/mdt.R
@@ -16,14 +16,17 @@ mdt <- function(num_items = 20L,
               is.null(feedback))
   media_dir <- gsub("/$", "", media_dir)
 
-  psychTestR::new_timeline(c(
-    if (take_training) instructions(media_dir, num_items),
-    main_test(label = label, media_dir = media_dir, num_items = num_items,
-              next_item.criterion = next_item.criterion,
-              next_item.estimator = next_item.estimator,
-              final_ability.estimator = final_ability.estimator,
-              constrain_answers = constrain_answers),
-    feedback
-  ),
-  dict = dict)
+  psychTestR::new_timeline({
+    browser()
+    c(
+      if (take_training) instructions(media_dir, num_items),
+      main_test(label = label, media_dir = media_dir, num_items = num_items,
+                next_item.criterion = next_item.criterion,
+                next_item.estimator = next_item.estimator,
+                final_ability.estimator = final_ability.estimator,
+                constrain_answers = constrain_answers)
+      # feedback
+      stop("need to write method for c.test_element that produces timelines")
+    )},
+    dict = dict)
 }
