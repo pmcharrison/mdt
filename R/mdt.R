d49413ce62ff96a6705a2691943304e7a293e576
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-07 13:25:51 +0100

    Removing debugging code from mdt()

--- R/mdt.R
+++ R/mdt.R
@@ -17,17 +17,14 @@ mdt <- function(num_items = 20L,
   media_dir <- gsub("/$", "", media_dir)
 
   psychTestR::new_timeline({
-    browser()
     c(
       if (take_training) instructions(media_dir, num_items),
       main_test(label = label, media_dir = media_dir, num_items = num_items,
                 next_item.criterion = next_item.criterion,
                 next_item.estimator = next_item.estimator,
                 final_ability.estimator = final_ability.estimator,
-                constrain_answers = constrain_answers)
-      # feedback
-      stop("new_timeline should be able to accept input lists that ",
-           "contain timelines")
+                constrain_answers = constrain_answers),
+      feedback
     )},
     dict = dict)
 }
