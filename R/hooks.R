55960f166c8fc6c5ede667c6f50fc9e7efe22ffc
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-26 09:24:40 +0100

    Updating date

--- R/hooks.R
+++ R/hooks.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   packageStartupMessage(
-    "Note: on September 25th 2018, the following change was made to ",
+    "Note: on September 26th 2018, the following change was made to ",
     "default arguments for mdt(), demo_mdt(), and standalone_mdt():\n",
     "- next_item.estimator now defaults to 'BM', not 'WL'.\n"
   )
