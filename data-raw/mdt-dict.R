4cab204eec3cb24be90b81c35ce127f508981386
Peter Harrison <pmch2@cam.ac.uk>
2022-03-25 15:58:58 +0000

    Add UTF-8 encoding for dictionaries

--- data-raw/mdt-dict.R
+++ data-raw/mdt-dict.R
@@ -1,25 +1,25 @@
-input <- read.csv("data-raw/mdt-dict.csv", stringsAsFactors = FALSE)
+input <- read.csv("data-raw/mdt-dict.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(input)[[1]] <- "key"
 
-russian <- read.csv("data-raw/dict-russian.csv", stringsAsFactors = FALSE)
+russian <- read.csv("data-raw/dict-russian.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(russian)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == russian$key))
 input$RU <- russian$RU
 
-italian <- read.csv("data-raw/dict-italian.csv", stringsAsFactors = FALSE)
+italian <- read.csv("data-raw/dict-italian.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(italian)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == italian$key))
 input$IT <- italian$IT
 
-german_formal <- read.csv("data-raw/dict-german-formal.csv", stringsAsFactors = FALSE)
+german_formal <- read.csv("data-raw/dict-german-formal.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(german_formal)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == german_formal$key))
 input$DE_F <- german_formal$DE_F
 
-spanish <- read.csv("data-raw/dict-spanish.csv", stringsAsFactors = FALSE)
+spanish <- read.csv("data-raw/dict-spanish.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(spanish)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == spanish$key))
